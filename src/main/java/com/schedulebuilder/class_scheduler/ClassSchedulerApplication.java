47fcc49b4ed6a9351b1f50e96dd8b9057943c754
Jeremiah Baccam <jjbaccam@iastate.edu>
2024-11-16 13:07:07 -0600

    Initial Commmit

---
+++ src/main/java/com/schedulebuilder/class_scheduler/ClassSchedulerApplication.java
@@ -0,0 +1,13 @@
+package com.schedulebuilder.class_scheduler;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClassSchedulerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClassSchedulerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/schedulebuilder/class_scheduler/ClassSchedulerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.schedulebuilder.class_scheduler;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClassSchedulerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
