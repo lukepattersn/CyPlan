14c910c32b89b661f373e67d978c15c964ad45ee
Jeremiah Baccam <jjbaccam@iastate.edu>
2024-11-18 12:52:46 -0600

    possible solution for fixing CORS issue

--- src/main/java/com/schedulebuilder/class_scheduler/ClassSchedulerApplication.java
+++ src/main/java/com/schedulebuilder/class_scheduler/ClassSchedulerApplication.java
@@ -2,6 +2,9 @@ package com.schedulebuilder.class_scheduler;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class ClassSchedulerApplication {
@@ -10,4 +13,25 @@ public class ClassSchedulerApplication {
 		SpringApplication.run(ClassSchedulerApplication.class, args);
 	}
 
+	@Bean
+	public WebMvcConfigurer corsConfigurer() {
+		return new WebMvcConfigurer() {
+			@Override
+			public void addCorsMappings(CorsRegistry registry) {
+				registry.addMapping("/**")
+					.allowedOrigins(
+						"https://cy-plan.com",
+						"https://www.cy-plan.com",
+						"https://cyplan-d380797cab33.herokuapp.com"
+					)
+					.allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+					.allowedHeaders("*")
+					.allowCredentials(true)
+					.maxAge(3600);
+			}
+		};
+	}
+
 }
+
+
